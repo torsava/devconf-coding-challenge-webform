89b0a51e7443376794875d4ca014503e69ee847a
Miro Hrončok <miro@hroncok.cz>
2017-06-02 14:57:23 +0200

    Add WSGI entrypoint for OpenShift

---
+++ wsgi.py
@@ -0,0 +1,7 @@
+import os
+
+from form import app as application
+
+if 'OPENSHIFT_DATA_DIR' in os.environ:
+    datadir = os.environ['OPENSHIFT_DATA_DIR']
+    application.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + datadir + '/form.db'
